0d79b258664a6f46d42e398e3daa0ea6dee9e15b
Ankan Bag <91788199+akbng@users.noreply.github.com>
2023-05-12 19:05:47 +0530

    update rollup entry & manifest for the popup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-// import copy from 'rollup-plugin-copy';
+import copy from 'rollup-plugin-copy';
 
 export default [
   {
@@ -19,6 +19,53 @@ export default [
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       terser(),
+    ],
+  },
+
+  {
+    input: 'src/popups/scripts/index.js',
+    output: {
+      file: 'dist/popup.js',
+      format: 'esm',
+    },
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      // typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+      copy({
+        targets: [
+          {
+            src: 'src/popups/index.html',
+            dest: 'dist',
+            rename: 'popup.html',
+            transform: (contents) => {
+              return contents
+                .toString()
+                .replace('styles/index.css', 'popup.css')
+                .replace('scripts/index.js', 'popup.js')
+                .replace('../../public/keys.png', 'assets/keys.png');
+            },
+          },
+          {
+            src: 'src/popups/styles/index.css',
+            dest: 'dist',
+            rename: 'popup.css',
+          },
+          {
+            src: 'public/keys.png',
+            dest: 'dist/assets',
+          },
+          {
+            src: 'manifest.json',
+            dest: 'dist',
+            transform: (contents) => {
+              return contents.toString().replace(/dist\//g, '');
+            },
+          },
+        ],
+      }),
     ],
   },
 ];
